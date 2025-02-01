8473058105035ca20c6fde01d61d8ee7d2988714
DarkWinged <James.Levi.Rogers@gmail.com>
2025-02-01 03:27:18 -0600

    fix: formatting fixes

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,9 +1,10 @@
 import random as rand
 from src.generation import one_d
 
+
 def test_one_d():
-	expected = [1, 4, 7, 7, 7, 4, 8, 5, 0, 4]
-	rand.seed(100)
-	result = one_d(10)
-	assert len(result) == 10
-	assert result == expected
+    expected = [1, 4, 7, 7, 7, 4, 8, 5, 0, 4]
+    rand.seed(100)
+    result = one_d(10)
+    assert len(result) == 10
+    assert result == expected
