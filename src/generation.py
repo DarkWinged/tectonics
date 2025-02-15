e8027123c23ec252319ee0cc03786b5df450d737
DarkWinged <James.Levi.Rogers@gmail.com>
2025-02-14 20:37:25 -0600

    fix: forgot to add a file

--- src/generation.py
+++ src/generation.py
@@ -1,6 +1,7 @@
-from random import random
-from typing import List
+from random import randrange
+from typing import List, Union
 
-
-def one_d(n: int) -> List[int]:
-    return [max(int(random() * 10), 1) for n in range(n)]
+def n_d(*dims: int, low: int = 0, high: int = 10) -> Union[int, List]:
+    if not dims:
+        return randrange(low, high, 1)
+    return [n_d(*dims[1:], low=low, high=high) for _ in range(dims[0])]
