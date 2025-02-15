4d356992c1b9e3e71eb0361e967104417eb08140
DarkWinged <James.Levi.Rogers@gmail.com>
2025-02-14 20:40:22 -0600

    test: forgot to include updated tests

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,9 +1,9 @@
 import random as rand
-from src.generation import one_d
+from src.generation import n_d
 
 
-def test_one_d():
+def test_n_d():
     rand.seed(100)
-    result = one_d(10)
+    result = n_d(10)
     assert len(result) == 10
     assert min(result) >= 1
